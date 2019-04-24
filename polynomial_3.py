5910533cff7cae8f9c67b3aab449993ec0154293
lewyctrl123@gmail.com <lewyctrl123@gmail.com>
2019-04-24 13:34:28 +0200

    Monomial -> Coefficient
    ...

--- polynomial_3.py
+++ polynomial_3.py
@@ -1,6 +1,6 @@
 class Polynomial_3:
     def __init__(self, a, b, c, d):
-        self.monomials = [a, b, c, d]
+        self.coefficients = [a, b, c, d]
 
     # print polynomial in standard form
     def printPoly(self):
@@ -8,11 +8,11 @@ class Polynomial_3:
 
     # returns polynomial string in standard form, based on self.factors
     def getString(self):
-        handledMonomials = list( map(self.handleMonomial, self.monomials) )
-        return self.joinMonomials(handledMonomials)
+        handledCoefficients = list( map(self.handleCoefficient, self.coefficients) )
+        return self.joinCoefficients(handledCoefficients)
     
     # prepare single polynomial factor to be in proper way to be printed
-    def handleMonomial(self, x):
+    def handleCoefficient(self, x):
         result = str(x)
         if x == -1: result = '-'
         elif x == 1: result = '+'
@@ -20,12 +20,12 @@ class Polynomial_3:
         return result
 
     # create string from prepared factors
-    def joinMonomials(self, list_factor):
+    def joinCoefficients(self, list_factor):
         result = ''
-        monomialDegree = 3
-        for currentMonomial in list_factor:
-            if currentMonomial != 0: result += currentMonomial + 'x^' + str(monomialDegree)
-            monomialDegree -= 1
+        coefficientDegree = 3
+        for currentCoefficient in list_factor:
+            if currentCoefficient != 0: result += currentCoefficient + 'x^' + str(coefficientDegree)
+            coefficientDegree -= 1
         result = result.replace('x^0', '')
         result = result.replace('x^1', 'x')
         if result[0] == '+': result = result[1:]
